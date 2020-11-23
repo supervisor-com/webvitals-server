88d0fbf784835d8f8459df6f1423fdc5942d0c0b
Matti Paksula <matti.paksula@iki.fi>
2020-11-23 18:25:07 +0200

    log, styling

--- cmd/webvitals-server/main.go
+++ cmd/webvitals-server/main.go
@@ -20,6 +20,7 @@ func main() {
 		lcp, _ := time.ParseDuration(c.DefaultQuery("lcp", "2.0s"))
 		cls, _ := time.ParseDuration(c.DefaultQuery("cls", "2.5s"))
 
+		highlight := c.DefaultQuery("highlight", "750")
 		timestamp := c.DefaultQuery("timestamp", strconv.FormatInt(time.Now().UTC().UnixNano(), 10))
 		seq, _ := strconv.Atoi(c.DefaultQuery("seq", "1"))
 		nextSeq := seq + 1
@@ -35,6 +36,7 @@ func main() {
 			"timestamp": timestamp,
 			"seq":       nextSeq,
 			"fcpMillis": fcpMillis,
+			"highlight": highlight,
 		})
 	})
 
